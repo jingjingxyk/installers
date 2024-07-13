80469e81e2dbb46a01eac5590110075b76581f42
jingjingxyk <zonghengbaihe521@qq.com>
2024-07-13 14:53:42 +0800

    添加 容器测试环境

---
+++ tools/scripts/run-alpine-container.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -exu
+__DIR__=$(
+  cd "$(dirname "$0")"
+  pwd
+)
+__PROJECT__=$(
+  cd ${__DIR__}/../../
+  pwd
+)
+cd ${__DIR__}
+
+{
+  docker stop alpine-dev
+  sleep 5
+} || {
+  echo $?
+}
+cd ${__DIR__}
+
+IMAGE=alpine:3.18
+
+MIRROR=''
+while [ $# -gt 0 ]; do
+  case "$1" in
+  --mirror)
+    MIRROR="$2"
+    case "$MIRROR" in
+      china | openatom)
+        IMAGE="hub.atomgit.com/library/alpine:3.18"
+        ;;
+    esac
+    ;;
+  esac
+  shift $(($# > 0 ? 1 : 0))
+done
+
+cd ${__DIR__}
+docker run --rm --name alpine-dev -d -v ${__PROJECT__}:/work -w /work $IMAGE tail -f /dev/null
---
+++ tools/scripts/run-rhel-container.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+set -exu
+__DIR__=$(
+  cd "$(dirname "$0")"
+  pwd
+)
+__PROJECT__=$(
+  cd ${__DIR__}/../../
+  pwd
+)
+cd ${__DIR__}
+
+{
+  docker stop rhel-dev
+  sleep 5
+} || {
+  echo $?
+}
+cd ${__DIR__}
+
+# IMAGE=oraclelinux:9
+
+IMAGE=almalinux:9
+IMAGE=rockylinux:9
+
+OS="rocky"
+MIRROR=''
+while [ $# -gt 0 ]; do
+  case "$1" in
+  --os)
+    OS="$2"
+      ;;
+  --mirror)
+    MIRROR="$2"
+    ;;
+  esac
+  shift $(($# > 0 ? 1 : 0))
+done
+
+
+
+case "$OS" in
+    rocky)
+      IMAGE=rockylinux:9
+      case "$MIRROR" in
+            china | openatom)
+              IMAGE="hub.atomgit.com/library/rockylinux:9"
+              ;;
+      esac
+      ;;
+    alma)
+      IMAGE=almalinux:9
+      case "$MIRROR" in
+            china | openatom)
+              IMAGE="hub.atomgit.com/library/almalinux:9"
+              ;;
+      esac
+      ;;
+esac
+
+
+cd ${__DIR__}
+docker run --rm --name rhel-dev -d -v ${__PROJECT__}:/work -w /work $IMAGE tail -f /dev/null
