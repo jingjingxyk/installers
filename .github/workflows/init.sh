570a4eef9001e2b3aa076ee138a6fdcba5611b9a
jingjingxyk <zonghengbaihe521@qq.com>
2024-07-14 16:50:32 +0800

    update install.sh

--- .github/workflows/init.sh
+++ .github/workflows/init.sh
@@ -1,6 +1,6 @@
 OS_RELEASE=$(awk -F= '/^ID=/{print $2}' /etc/os-release | tr -d '\n' | tr -d '\"')
 case "$OS_RELEASE" in
-'rocky' | 'almalinux' | 'alinux' | 'anolis' | 'fedora')
+'rocky' | 'almalinux' | 'alinux' | 'anolis' | 'fedora' | 'rhel' )
   yum update -y
   yum install -y which
   ;;
