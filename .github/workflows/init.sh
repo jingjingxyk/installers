bf8686f9a5fae5dbc42a4fb354069ace2a1f84f2
jingjingxyk <zonghengbaihe521@qq.com>
2024-07-14 16:17:58 +0800

    update workflow

---
+++ .github/workflows/init.sh
@@ -0,0 +1,15 @@
+
+OS_RELEASE=$(awk -F= '/^ID=/{print $2}' /etc/os-release | tr -d '\n' | tr -d '\"')
+case "$OS_RELEASE" in
+'rocky' )
+  yum update -y
+  yum install -y which
+  ;;
+'ubuntu')
+  sed -i.bak "s@security.ubuntu.com@azure.archive.ubuntu.com@g" /etc/apt/sources.list
+  sed -i.bak "s@archive.ubuntu.com@azure.archive.ubuntu.com@g" /etc/apt/sources.list
+  ;;
+'alpine')
+  apk update
+  apk add bash
+esac
